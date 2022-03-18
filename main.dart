561a5915cc5580587a3b2025aa7f3c07f76b7ba7
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-03-18 16:44:23 +0530

    taking inputs

--- main.dart
+++ main.dart
@@ -1,7 +1,15 @@
-
+import 'dart:io';
+import 'dart:async';
 void main(List<String> args) {
 
   //simple printing
   print("Hello Dart from Lakshit");
+
+  // taking inputs
+  int a;
+  stdout.write("Enter a number : ");
+  a = int.parse(stdin.readLineSync()!);
+
+  print("Your number is ${a}");
 
 }
