ecc773c54de06c629dca5e4748c683ca966c6391
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-03-18 16:49:13 +0530

    variables and string interpolation

--- main.dart
+++ main.dart
@@ -8,8 +8,13 @@ void main(List<String> args) {
   // taking inputs
   int a;
   stdout.write("Enter a number : ");
-  a = int.parse(stdin.readLineSync()!);
+  a = int.parse(stdin.readLineSync()!);//! assures dart compiler that there won't be any exception
 
   print("Your number is ${a}");
+
+  //variables
+  int marks = 95;
+  String name = "Lakshit";
+  print("$name scored $marks marks"); //string interpolation
 
 }
