4419ae3b13a11b31258502b29dd3c5be29254642
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-03-21 22:50:17 +0530

    if-else

--- main3.dart
+++ main3.dart
@@ -4,5 +4,16 @@ void main(List<String> args) {
   int x = 75;
   assert(x == 75);//if false it will result in exception else the code flow will continue
 
+  //if-else
+  int y = 64,z = 85;
+  if(y > z){
+    print('$y greater');
+  }
+  else if(y < z){
+    print('$z greater');
+  }
+  else{
+    print('equal');
+  }
 
 }
