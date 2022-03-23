392e6add0dee78c4ec17a10c32467371ba2d8df5
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-03-23 21:04:18 +0530

    optional argument functions

--- main4.dart
+++ main4.dart
@@ -4,6 +4,11 @@ void main(List<String> args) {
 
   print("Sum of 5 and 7 is ${sum(5,7)}");
   print("Area of 4.76 units length and 3.13 units breadth is ${area(4.76,3.13)}");
+
+  printFileName("hello.java");
+  printFileName();
+  print(sumAll(5, 2,8));
+  print(sumAll(6,2));
 }
 
 //normal functions
@@ -23,4 +28,13 @@ int sum(int a,int b){
 
 double area(double length,double breadth){
   return length*breadth;
+}
+
+
+//optional arguments funtions
+void printFileName([String fname='test.txt']){
+  print("filename is $fname");
+}
+int sumAll(int a,int b,[int c = 0]){//optional arguments come at the last in declaration or definition
+  return a+b+c;
 }
