f8b6557ff0ba2c8def702a9d058643fd0c4b2330
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-03-23 20:44:56 +0530

    funs with params

--- main4.dart
+++ main4.dart
@@ -1,9 +1,15 @@
 void main(List<String> args) {
   greetUser();
+  greetName("Karl");
 }
 
 //normal functions
 void greetUser(){
   print("Hello User");
+}
+
+//functions with parameters
+void greetName(String name){
+  print("Hello $name");
 }
 
