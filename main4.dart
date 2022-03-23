c608eccb8f67ffafc01d86d1579d6f914961e913
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-03-23 20:50:52 +0530

    calculating functions

--- main4.dart
+++ main4.dart
@@ -1,6 +1,9 @@
 void main(List<String> args) {
   greetUser();
   greetName("Karl");
+
+  print("Sum of 5 and 7 is ${sum(5,7)}");
+  print("Area of 4.76 units length and 3.13 units breadth is ${area(4.76,3.13)}");
 }
 
 //normal functions
@@ -13,3 +16,11 @@ void greetName(String name){
   print("Hello $name");
 }
 
+//calculating functions
+int sum(int a,int b){
+  return a+b;
+}
+
+double area(double length,double breadth){
+  return length*breadth;
+}
